fcf744b00ab89d90405593b17d855796c082043e
WesleyClements <wesley.clements@gmail.com>
2020-03-08 16:01:14 -0700

    added draw utility function

--- src/util/LineSegment.ts
+++ src/util/LineSegment.ts
@@ -1,18 +1,27 @@
 import Vector2 from './Vector2.js';
 export default class LineSegment {
-  start: Vector2;
-  end: Vector2;
+  static draw(a: Vector2, b: Vector2): void {
+    if (!a || !b) return;
+    line(a.x, a.y, b.x, b.y);
+  }
+
+  a: Vector2;
+  b: Vector2;
 
   constructor(start: Vector2, end: Vector2) {
-    this.start = start;
-    this.end = end;
+    this.a = start;
+    this.b = end;
   }
 
-  get length() {
-    return Vector2.distance(this.start, this.end);
+  get length(): number {
+    return Vector2.distance(this.a, this.b);
   }
 
-  get midpoint() {
-    return Vector2.midpoint(this.start, this.end);
+  get midpoint(): Vector2 {
+    return Vector2.midpoint(this.a, this.b);
+  }
+
+  draw(): void {
+    LineSegment.draw(this.a, this.b);
   }
 }
