a8f2d9b3e018c8427896a8fc795346ce03e8ca22
WesleyClements <wesley.clements@gmail.com>
2020-03-08 20:30:56 -0700

    added bounding AABB utility functions

--- src/util/LineSegment.ts
+++ src/util/LineSegment.ts
@@ -1,8 +1,17 @@
 import Vector2 from './Vector2.js';
+import AABB from './AABB.js';
+
 export default class LineSegment {
   static draw(a: Vector2, b: Vector2): void {
     if (!a || !b) return;
     line(a.x, a.y, b.x, b.y);
+  }
+
+  static getBoundingAABB(a: Vector2, b: Vector2): AABB {
+    return new AABB(
+      new Vector2(Math.min(a.x, b.x), Math.min(a.y, b.y)),
+      new Vector2(Math.max(a.x, b.x), Math.max(a.y, b.y)),
+    );
   }
 
   a: Vector2;
@@ -19,6 +28,10 @@ export default class LineSegment {
 
   get midpoint(): Vector2 {
     return Vector2.midpoint(this.a, this.b);
+  }
+
+  get boundingAABB(): AABB {
+    return LineSegment.getBoundingAABB(this.a, this.b);
   }
 
   draw(): void {
