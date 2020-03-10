b91926ff4762558b22cdab398fffebe16fba7898
WesleyClements <wesley.clements@gmail.com>
2020-03-09 20:38:28 -0700

    implemented intersection test between LineSegment and AABB

--- src/util/AABB.ts
+++ src/util/AABB.ts
@@ -1,4 +1,6 @@
 import Vector2 from './Vector2.js';
+import LineSegment from './LineSegment.js';
+import { isBetween } from './FloatUtil.js';
 
 export default class AABB {
   static clone(aabb: AABB): AABB {
@@ -19,6 +21,10 @@ export default class AABB {
     this.max = max;
   }
 
+  get center(): Vector2 {
+    return Vector2.midpoint(this.min, this.max);
+  }
+
   get width(): number {
     return this.max.x - this.min.x;
   }
@@ -26,34 +32,60 @@ export default class AABB {
     return this.max.y - this.min.y;
   }
 
-  get center(): Vector2 {
-    return Vector2.midpoint(this.min, this.max);
+  get top(): LineSegment {
+    return new LineSegment(new Vector2(this.min.x, this.max.y), Vector2.clone(this.max));
+  }
+  get bottom(): LineSegment {
+    return new LineSegment(Vector2.clone(this.min), new Vector2(this.max.x, this.min.y));
+  }
+  get left(): LineSegment {
+    return new LineSegment(Vector2.clone(this.min), new Vector2(this.min.x, this.max.y));
+  }
+  get right(): LineSegment {
+    return new LineSegment(new Vector2(this.max.x, this.min.y), Vector2.clone(this.max));
   }
 
-  contains(obj: Vector2 | AABB): boolean {
+  contains(obj: Vector2 | LineSegment | AABB): boolean {
     if (obj == null) throw new Error('obj is null');
     if (obj instanceof Vector2) {
-      if (obj.x < this.min.x) return false;
-      if (obj.x > this.max.x) return false;
-      if (obj.y < this.min.y) return false;
-      if (obj.y > this.max.y) return false;
-      return true;
+      return isBetween(obj.x, this.min.x, this.max.x) && isBetween(obj.y, this.min.y, this.max.y);
+    } else if (obj instanceof LineSegment) {
+      return this.contains(obj.a) && this.contains(obj.b);
     } else if (obj instanceof AABB) {
       return this.contains(obj.min) && this.contains(obj.max);
-    }
-    throw new Error('not implemented yet');
+    } else throw new Error('not implemented yet');
   }
 
-  intersects(obj: AABB): boolean {
+  intersects(obj: LineSegment | AABB): boolean {
     if (obj == null) throw new Error('obj is null');
+    if (obj instanceof LineSegment) {
+      if (Math.max(obj.a.x, obj.b.x) < this.min.x) return false;
+      if (Math.min(obj.a.x, obj.b.x) > this.max.x) return false;
+      if (Math.max(obj.a.y, obj.b.y) < this.min.y) return false;
+      if (Math.min(obj.a.y, obj.b.y) > this.max.y) return false;
+      if (this.contains(obj.a) || this.contains(obj.b)) return true;
+      if (isBetween(obj.a.x, this.min.x, this.max.x) && isBetween(obj.b.x, this.min.x, this.max.x)) return true;
+      if (isBetween(obj.a.y, this.min.y, this.max.y) && isBetween(obj.b.y, this.min.y, this.max.y)) return true;
+      if (this.top.intersects(obj)) return true;
+      if (this.bottom.intersects(obj)) return true;
+      if (this.left.intersects(obj)) return true;
+      if (this.right.intersects(obj)) return true;
+      return false;
+    }
     if (obj instanceof AABB) {
       if (obj.max.x < this.min.x) return false;
       if (obj.min.x > this.max.x) return false;
       if (obj.max.y < this.min.y) return false;
       if (obj.min.y > this.max.y) return false;
       return true;
-    }
-    throw new Error('not implemented yet');
+    } else throw new Error('not implemented yet');
+  }
+
+  clamp(obj: Vector2 | LineSegment): Vector2 | LineSegment {
+    if (obj == null) throw new Error('obj is null');
+    if (obj instanceof Vector2) {
+      return new Vector2(constrain(obj.x, this.min.x, this.max.x), constrain(obj.y, this.min.y, this.max.y));
+    } else throw new Error('not implemented yet');
   }
 
   draw(): void {
--- src/util/LineSegment.ts
+++ src/util/LineSegment.ts
@@ -1,4 +1,5 @@
 import Vector2 from './Vector2.js';
+import Line from './Line.js';
 import AABB from './AABB.js';
 
 export default class LineSegment {
@@ -22,6 +23,14 @@ export default class LineSegment {
     this.b = end;
   }
 
+  get AB(): Vector2 {
+    return Vector2.subtract(this.b, this.a);
+  }
+
+  get BA(): Vector2 {
+    return Vector2.subtract(this.a, this.b);
+  }
+
   get length(): number {
     return Vector2.distance(this.a, this.b);
   }
@@ -30,8 +39,37 @@ export default class LineSegment {
     return Vector2.midpoint(this.a, this.b);
   }
 
+  get line(): Line {
+    return new Line(this.a, this.b);
+  }
+
   get boundingAABB(): AABB {
     return LineSegment.getBoundingAABB(this.a, this.b);
+  }
+
+  intersects(obj: LineSegment | AABB): boolean {
+    if (obj == null) throw new Error('obj is null');
+    if (obj instanceof LineSegment) {
+      const aabbA = this.boundingAABB;
+      const aabbB = obj.boundingAABB;
+      if (!aabbA.intersects(aabbB)) return false;
+
+      const ab = this.AB;
+      const testAC = Vector2.cross(ab, Vector2.subtract(obj.a, this.a));
+      const testAD = Vector2.cross(ab, Vector2.subtract(obj.b, this.a));
+      if (Math.max(testAC, testAD) < 0) return false;
+      if (Math.min(testAC, testAD) > 0) return false;
+
+      const cd = obj.AB;
+      const testCA = Vector2.cross(cd, Vector2.subtract(this.a, obj.a));
+      const testCB = Vector2.cross(cd, Vector2.subtract(this.b, obj.a));
+      if (Math.max(testCA, testCB) < 0) return false;
+      if (Math.min(testCA, testCB) > 0) return false;
+
+      return true;
+    } else if (obj instanceof AABB) {
+      return obj.intersects(this);
+    } else throw new Error('not implemented yet');
   }
 
   draw(): void {
